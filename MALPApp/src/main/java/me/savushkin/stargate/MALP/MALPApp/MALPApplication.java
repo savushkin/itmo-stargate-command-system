346395e6936f0d1282cc531552e9e359fedc074f
Ivan Savushkin <ivan@savushkin.me>
2017-12-18 17:28:34 +0300

    initial commit

---
+++ MALPApp/src/main/java/me/savushkin/stargate/MALP/MALPApp/MALPApplication.java
@@ -0,0 +1,12 @@
+package me.savushkin.stargate.MALP.MALPApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MALPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MALPApplication.class, args);
+	}
+}
