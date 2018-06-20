16fb663b91750ea3f476d9c60116399181bce794
Ivan Savushkin <ivan@savushkin.me>
2018-06-20 04:30:00 +0300

    stargate control component

--- baseFrontend/src/app/_service/stargate/stargate.service.ts
+++ baseFrontend/src/app/_service/stargate/stargate.service.ts
@@ -1,11 +1,8 @@
 import {Injectable} from '@angular/core';
-import {User} from "@sgc/_model/user";
-import {Page} from "@sgc/_model/page";
 import {Observable} from "rxjs/Observable";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {environment} from "@env/environment";
 import {Command} from "@sgc/_model/command";
-import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {Subject} from "rxjs/Subject";
 
 @Injectable()
@@ -46,7 +43,8 @@ export class StargateService {
 
   public loadState() {
     this.getStarGateState().subscribe(
-      state => this.state.next(state)
+      state => this.state.next(state),
+      error => this.state.next(null)
     );
   }
 
